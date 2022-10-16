803104ea834968856a70392c793474137fa906a1
Andrew Gettz <drews_computer@Andrews-MacBook-Pro.local>
2022-10-16 15:23:38 -0400

    fixed async in seedAll

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,7 @@ const seedComments = require('./comment-seeds');
 
 const sequelize =('../config/connection'); 
 
-const seedAll = sync() => {
+const seedAll = async() => {
         await sequelize.sync({force: true}); 
         await seedUsers();
         await seedPosts(); 
